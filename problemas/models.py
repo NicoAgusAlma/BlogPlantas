96d63367799054bfb3f9fdef1258de08dd94e339
Nicolas Almazan <seguroseteolvida@gmail.com>
2022-06-11 00:16:39 -0300

    vinculados mas modelos

--- problemas/models.py
+++ problemas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from productos.models import Producto
 
 # Create your models here.
 class Problema(models.Model):
@@ -7,7 +8,7 @@ class Problema(models.Model):
     nombreCientifico=models.CharField(max_length=50)
     imagen=models.ImageField(upload_to='imagenesProblemas', null=True, blank=True)
     peligro=models.CharField(help_text='Bajo, Medio o Alto', max_length=10)
-    productos=models.CharField(help_text='Productos de ayuda.', max_length=50)
+    productos=models.ForeignKey(Producto, on_delete=models.CASCADE)
     solucion=models.CharField(help_text='Manera de solucionarlo', max_length=2000)
 
     def __str__(self):
--- productos/models.py
+++ productos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from viveros.models import Vivero
 
 # Create your models here.
 class Producto(models.Model):
@@ -7,7 +8,7 @@ class Producto(models.Model):
     precio=models.IntegerField(help_text='Precio en U$s blue.')
     imagen=models.ImageField(upload_to='imagenesProductos', null=True, blank=True)
     solucionaProblemas=models.CharField(help_text='Problemas que soluciona.', max_length=200)
-    puntoDeVenta=models.CharField(help_text='Viveros donde comprarlo.', max_length=200)
+    puntoDeVenta=models.ForeignKey(Vivero, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.nombre}'
