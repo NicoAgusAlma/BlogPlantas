eff0da12a9047a6883df6a5f80ff6c935d9e49ea
Nicolas Almazan <seguroseteolvida@gmail.com>
2022-06-13 13:20:19 -0300

    solucionado buscador

--- busqueda/views.py
+++ busqueda/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
 from blog.models import Posteo
-# from plantas.models import Planta
+from plantas.models import Planta
 from viveros.models import Vivero
 from productos.models import Producto
 from problemas.models import Problema
@@ -18,38 +18,31 @@ def buscar(request):
     if request.GET['busqueda']:
         search_param = request.GET['busqueda']
         print(search_param)
-        # query = Q(nombreComun__contains=search_param)
-        # query.add(Q(nombreCientifico__contains=search_param), Q.OR)
-        # query.add(Q(familia__contains=search_param), Q.OR)
-        # query.add(Q(sustrato__contains=search_param), Q.OR)
-        # query.add(Q(viveros__contains=search_param), Q.OR)
-        # query.add(Q(peligrosComunes__contains=search_param), Q.OR)
-        # plantas = Planta.objects.filter(query)
+        query = Q(nombreComun__contains=search_param)
+        query.add(Q(nombreCientifico__contains=search_param), Q.OR)
+        query.add(Q(familia__contains=search_param), Q.OR)
+        query.add(Q(sustrato__contains=search_param), Q.OR)        
+        plantas = Planta.objects.filter(query)
         query = Q(nombreProblema__contains=search_param)
         query.add(Q(nombreCientifico__contains=search_param), Q.OR)
-        query.add(Q(peligro__contains=search_param), Q.OR)
-        query.add(Q(productos__contains=search_param), Q.OR)
+        query.add(Q(peligro__contains=search_param), Q.OR)       
         query.add(Q(solucion__contains=search_param), Q.OR)        
         problemas = Problema.objects.filter(query)
         query = Q(nombre__contains=search_param)
         query.add(Q(provincia__contains=search_param), Q.OR)
         query.add(Q(localidad__contains=search_param), Q.OR)
-        query.add(Q(calle__contains=search_param), Q.OR)
-        query.add(Q(stockPlantas__contains=search_param), Q.OR)        
-        query.add(Q(stockProductos__contains=search_param), Q.OR)             
+        query.add(Q(calle__contains=search_param), Q.OR)           
         viveros = Vivero.objects.filter(query)
-        query = Q(nombre__contains=search_param)
-        query.add(Q(solucionaProblemas__contains=search_param), Q.OR)
-        query.add(Q(puntoDeVenta__contains=search_param), Q.OR)               
+        query = Q(nombre__contains=search_param)              
         productos = Producto.objects.filter(query)            
         query = Q(titulo__contains=search_param)        
         query.add(Q(subtitulo__contains=search_param), Q.OR)
         query.add(Q(texto__contains=search_param), Q.OR)               
-        query.add(Q(autor__username__contains=search_param), Q.OR)               
+        query.add(Q(categoria__contains=search_param), Q.OR)                     
         posteos = Posteo.objects.filter(query)
         context_dict = {
             'search_param':search_param,
-            #'plantas': plantas,
+            'plantas': plantas,
             'problemas': problemas,
             'viveros':viveros,
             'productos':productos,
