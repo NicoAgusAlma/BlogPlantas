ce58c14a93a6ec9f9ef2aa9dee54d5eaa04b6ad5
Nicolas Almazan <seguroseteolvida@gmail.com>
2022-06-13 02:53:03 -0300

    agregados forms de categorias

--- blog/forms.py
+++ blog/forms.py
@@ -2,12 +2,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
-from blog.models import Posteo, Comentario #, Categoria
+from blog.models import Posteo, Comentario, Categoria
 
-# cat = Categoria.objects.all().values_list('nombre', 'nombre')
-# choice_list = []
-# for item in cat:
-#     choice_list.append(item)
+cat = Categoria.objects.all().values_list('nombre', 'nombre')
+choice_list = []
+for item in cat:
+    choice_list.append(item)
 
 class BlogPost(forms.ModelForm):
     class Meta:
@@ -15,7 +15,7 @@ class BlogPost(forms.ModelForm):
         fields = ('titulo','subtitulo','autor', 'texto', 'categoria', 'imagen')
         widgets = {
             'autor': forms.TextInput(attrs={'palceholder':'user name', 'id':'usuarioImagen', 'type':'hidden'}),
-            #'categoria': forms.Select(choices=choice_list),
+            'categoria': forms.Select(choices=choice_list),
         }
 
 class Comentar(forms.ModelForm):
